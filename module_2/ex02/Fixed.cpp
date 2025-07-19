1c50e73c221894adc3cbcbae2fcb377b456557b3
HusrevAkbas <husrev.akbas@gmail.com>
2025-07-20 00:13:09 +0200

    feat: module 2 ex02 is done. comparison, arithmetic calculation, increment, decrement operators and max min methods added to Fixed class

---
+++ module_2/ex02/Fixed.cpp
@@ -0,0 +1,217 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: husrevakbas <husrevakbas@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 23:32:36 by husrevakbas       #+#    #+#             */
+/*   Updated: 2025/07/20 00:02:33 by husrevakbas      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+/**
+ * 		(1 << bits) = 2^bits = pow(2, bits)
+ */
+
+Fixed::Fixed()
+{	
+	//	std::cout << "Default constructor called" << std::endl;
+	this->value = 0;
+}
+
+Fixed::Fixed(const int value)
+{
+	//	std::cout << "Int constructor called" << std::endl;
+	if (value > static_cast<int32_t>(roundf(std::numeric_limits<int32_t>::max() / (1 << bits))))
+		std::cout << "\e[1;31mGiven value is greater than max representable value\e[0m" << std::endl;
+	if (value < static_cast<int32_t>(roundf(std::numeric_limits<int32_t>::min() / (1 << bits))))
+		std::cout << "\e[1;31mGiven value is lower than min representable value\e[0m" << std::endl;
+	this->value = value * (1 << bits);
+}
+
+Fixed::Fixed(const float value)
+{
+	//	std::cout << "Float constructor called" << std::endl;
+	if (value > std::numeric_limits<int32_t>::max() / (1 << bits))
+		std::cout << "\e[1;31mGiven value exeeds max representable value\e[0m" << std::endl;
+	if (value < std::numeric_limits<int32_t>::min() / (1 << bits))
+		std::cout << "\e[1;31mGiven value is lower than min representable value\e[0m" << std::endl;
+	this->value = roundf(value * ( 1 << bits));
+}
+
+Fixed::~Fixed()
+{	//	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &a)
+{
+	//	std::cout << "Copy constructor called" << std::endl;
+	*this = a;
+}
+
+Fixed& Fixed::operator=(const Fixed &a)
+{
+	//	std::cout << "Copy assignment operator called" << std::endl;
+	this->value = a.getRawBits();
+	return (*this);
+}
+
+Fixed& Fixed::operator+(const Fixed &a)
+{
+	//	std::cout << "Addition operator called" << std::endl;
+	this->value += a.getRawBits();
+	return (*this);
+}
+
+Fixed& Fixed::operator-(const Fixed &a)
+{
+	//	std::cout << "Subtraction operator called" << std::endl;
+	this->value -= a.getRawBits();
+	return (*this);
+}
+
+Fixed& Fixed::operator*(const Fixed &a)
+{
+	//	std::cout << "Multiplication operator called" << std::endl;
+	std::cout << "a: " << a.getRawBits() << " this: " << this->value << std::endl;
+	this->value *= a.getRawBits() / (1 << 8);
+	std::cout << "this after : " << this->value << " float: " << this->toFloat() <<std::endl;
+	return (*this);
+}
+
+Fixed& Fixed::operator/(const Fixed &a)
+{
+	//	std::cout << "Division operator called" << std::endl;
+	if (a.getRawBits() == 0)
+		std::cout << "\e[1;31mDivision by 0, be careful!\e[0m" << std::endl;
+	this->value /= a.getRawBits();
+	return (*this);
+}
+
+std::ostream& operator<<(std::ostream &o, const Fixed &a)
+{
+	o << a.toFloat();
+	return (o);
+}
+
+int	Fixed::getRawBits( void ) const
+{
+	//	std::cout << "getRawBits member function called" << std::endl;
+	return (this->value);
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+	//	std::cout << "setRawBits member function called" << std::endl;
+	this->value = raw;
+}
+
+float	Fixed::toFloat( void ) const
+{
+	return (static_cast<float>(this->value) / static_cast<float>(1 << bits));
+}
+
+int	Fixed::toInt( void ) const
+{
+	return (this->value >> bits);
+}
+
+bool	operator<(Fixed a, Fixed b)
+{
+	if (a.getRawBits() < b.getRawBits())
+		return (true);
+	return (false);
+}
+
+bool	operator>(Fixed a, Fixed b)
+{
+	if (a.getRawBits() > b.getRawBits())
+		return (true);
+	return (false);
+}
+
+bool	operator>=(Fixed a, Fixed b)
+{
+	if (a.getRawBits() >= b.getRawBits())
+		return (true);
+	return (false);
+}
+
+bool	operator<=(Fixed a, Fixed b)
+{
+	if (a.getRawBits() <= b.getRawBits())
+		return (true);
+	return (false);
+}
+
+bool	operator==(Fixed a, Fixed b)
+{
+	if (a.getRawBits() == b.getRawBits())
+		return (true);
+	return (false);
+}
+
+bool	operator!=(Fixed a, Fixed b)
+{
+	if (a.getRawBits() != b.getRawBits())
+		return (true);
+	return (false);
+}
+
+Fixed	&Fixed::operator++(int)
+{
+	Fixed	&temp = *this;
+	this->value += 1;
+	return (temp);
+}
+
+Fixed	&Fixed::operator++()
+{
+	this->value += 1;
+	return (*this);
+}
+
+Fixed	&Fixed::operator--(int)
+{
+	Fixed	&temp = *this;
+	this->value -= 1;
+	return (temp);
+}
+
+Fixed	&Fixed::operator--()
+{
+	this->value -= 1;
+	return (*this);
+}
+
+Fixed&	Fixed::min( Fixed &a, Fixed &b )
+{
+	if (a.getRawBits() < b.getRawBits())
+		return (a);
+	else
+		return (b);
+}
+Fixed&	Fixed::max( Fixed &a, Fixed &b )
+{
+	if (a.getRawBits() > b.getRawBits())
+		return (a);
+	else
+		return (b);
+}
+const Fixed&	Fixed::min( const Fixed &a, const Fixed &b )
+{
+	if (a.getRawBits() < b.getRawBits())
+		return (a);
+	else
+		return (b);
+}
+const Fixed&	Fixed::max( const Fixed &a, const Fixed &b )
+{
+	if (a.getRawBits() > b.getRawBits())
+		return (a);
+	else
+		return (b);
+}
---
+++ module_2/ex02/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: husrevakbas <husrevakbas@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/18 15:57:01 by husrevakbas       #+#    #+#             */
+/*   Updated: 2025/07/20 00:10:27 by husrevakbas      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main()
+{
+	Fixed a;
+	Fixed const b( 100 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+	a = Fixed( 1234.4321f );
+	a = Fixed( 10.600f );
+	a = a + b;
+	a = a / b;
+	std::cout << std::endl;
+	std::cout << "a -eq b " << (a == b) << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << std::endl;
+	Fixed aa;
+	Fixed const ba( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << aa << std::endl;
+	std::cout << ++aa << std::endl;
+	std::cout << aa << std::endl;
+	// std::cout << aa++ << std::endl;
+	std::cout << aa << std::endl;
+	std::cout << ba << std::endl;
+	std::cout << Fixed::max( aa, ba ) << std::endl;
+	std::cout << Fixed::min( b, c ) << std::endl;
+	return (0);
+}
