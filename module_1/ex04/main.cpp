d106e29365be34dfcb0dbbe87883bec024327c40
Husrev Akbas <huakbas@c3r6p7.42vienna.com>
2025-07-24 13:58:16 +0200

    feat: module 1 ex04 added a check if target file is exist and dont have write permission. structure chenged to be able to replace words with newline

--- module_1/ex04/main.cpp
+++ module_1/ex04/main.cpp
@@ -6,12 +6,13 @@
 /*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 22:28:17 by husrevakbas       #+#    #+#             */
-/*   Updated: 2025/07/23 16:01:24 by huakbas          ###   ########.fr       */
+/*   Updated: 2025/07/24 13:55:58 by huakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 int	main(int argc, char *argv[])
 {
@@ -19,8 +20,9 @@ int	main(int argc, char *argv[])
 	size_t	end;
 	std::string	ofilename;
 	std::string	line;
-	std::string	remove;
-	std::string	put;
+	std::string	to_remove;
+	std::string	to_put;
+	std::string	content;
 	std::ifstream	input_file;
 	std::ofstream	output_file;
 
@@ -33,31 +35,38 @@ int	main(int argc, char *argv[])
 	input_file.open(argv[1]);
 	if (!input_file)
 	{
-		std::cout << "File does not exist" << std::endl;
+		std::cout << "Input file does not exist" << std::endl;
 		return (0);
 	}
 	ofilename = argv[1];
 	ofilename += ".replace";
 	output_file.open(ofilename.c_str());
-	remove = argv[2];
-	put = argv[3];
+	if (!output_file)
+	{
+		std::cout << "Cannot open target file due to permissions" << std::endl;
+		return (0);
+	}
+	to_remove = argv[2];
+	to_put = argv[3];
+	content = "";
 	while (getline(input_file, line))
 	{
-		if (!input_file.eof())
-			line += "\n";
-		end = 0;
-		while (end < line.length() && line[end])
+		if (content != "" && !input_file.eof())
+			content += "\n";
+		content += line;
+	}
+	end = 0;
+	while (end < content.length() && content[end])
+	{
+		start = end;
+		end = content.find(to_remove, start);
+		if (end == std::string::npos)
+			output_file << content.substr(start);
+		else
 		{
-			start = end;
-			end = line.find(remove, start);
-			if (end == std::string::npos)
-				output_file << line.substr(start);
-			else
-			{
-				output_file << line.substr(start, end - start);
-				output_file << put;
-				end += remove.length();
-			}
+			output_file << content.substr(start, end - start);
+			output_file << to_put;
+			end += to_remove.length();
 		}
 	}
 	return (0);
