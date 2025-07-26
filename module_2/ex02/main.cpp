5a3cc2de8c5f86305d18c868698af0c8cf452980
HusrevAkbas <husrev.akbas@gmail.com>
2025-07-27 01:20:04 +0200

    test: module 2 ex02 more cases added

--- module_2/ex02/main.cpp
+++ module_2/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: husrevakbas <husrevakbas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 15:57:01 by husrevakbas       #+#    #+#             */
-/*   Updated: 2025/07/23 15:11:09 by huakbas          ###   ########.fr       */
+/*   Updated: 2025/07/27 01:17:57 by husrevakbas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,50 +23,90 @@ int	main()
 	std::cout << "b: " << b << std::endl;
 	std::cout << "c: " << c << std::endl;
 	std::cout << "d: " << d << std::endl;
+
 	std::cout << std::setw(20) << std::setfill('-') << "" << std::endl;
+
+	a = 0;
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	a = 10;
+
+	std::cout << std::setw(20) << std::setfill('-') << "" << std::endl;
+
 	std::cout << "a < b " << std::boolalpha << (a < b) << std::endl;
 	std::cout << "a > b " << (a > b) << std::endl;
+
 	std::cout << std::setw(20) << std::setfill('-') << "" << std::endl;
+
 	std::cout << "c == b " << (c == b) << std::endl;
 	std::cout << "d == b " << (d == b) << std::endl;
+
 	std::cout << std::setw(20) << std::setfill('-') << "" << std::endl;
-	std::cout << "d > b " << (d == b) << std::endl;
-	std::cout << "d >= b " << (d == b) << std::endl;
-	std::cout << "d <= b " << (d == b) << std::endl;
+
+	std::cout << "d > b " << (d > b) << std::endl;
+	std::cout << "d >= b " << (d >= b) << std::endl;
+	std::cout << "d <= b " << (d <= b) << std::endl;
+
 	std::cout << std::setw(20) << std::setfill('-') << "" << std::endl;
-	std::cout << "c > b " << (c == b) << std::endl;
-	std::cout << "c >= b " << (c == b) << std::endl;
-	std::cout << "c <= b " << (c == b) << std::endl;
+
+	std::cout << "c > b " << (c > b) << std::endl;
+	std::cout << "c >= b " << (c >= b) << std::endl;
+	std::cout << "c <= b " << (c <= b) << std::endl;
+
 	std::cout << std::setw(20) << std::setfill('-') << "" << std::endl;
+
 	d++;
-	std::cout << "d is increased by 1" << std::endl;
-	std::cout << "d > b " << (d == b) << std::endl;
-	std::cout << "d >= b " << (d == b) << std::endl;
-	std::cout << "d <= b " << (d == b) << std::endl;
+	std::cout << "d++: " << d << std::endl;
+	std::cout << "d > b " << (d > b) << std::endl;
+	std::cout << "d >= b " << (d >= b) << std::endl;
+	std::cout << "d <= b " << (d <= b) << std::endl;
+
 	std::cout << std::setw(20) << std::setfill('-') << "" << std::endl;
-	std::cout << "d is decreased by 1" << std::endl;
+
 	d--;
-	std::cout << "d > b " << (d == b) << std::endl;
-	std::cout << "d >= b " << (d == b) << std::endl;
-	std::cout << "d <= b " << (d == b) << std::endl;
+	std::cout << "d--: " << d << std::endl;
+	std::cout << "d > b " << (d > b) << std::endl;
+	std::cout << "d >= b " << (d >= b) << std::endl;
+	std::cout << "d <= b " << (d <= b) << std::endl;
+
 	std::cout << std::setw(20) << std::setfill('-') << "" << std::endl;
-	std::cout << "d is decreased by 1 again" << std::endl;
+
 	d--;
-	std::cout << "d > b " << (d == b) << std::endl;
-	std::cout << "d >= b " << (d == b) << std::endl;
-	std::cout << "d <= b " << (d == b) << std::endl;
+	std::cout << "d--: " << d << std::endl;
+	std::cout << "d > b " << (d > b) << std::endl;
+	std::cout << "d >= b " << (d >= b) << std::endl;
+	std::cout << "d <= b " << (d <= b) << std::endl;
+
 	std::cout << std::setw(20) << std::setfill('-') << "" << std::endl;
-	std::cout << "a set to 0 and increased by 1" << std::endl;
+
+	std::cout << "a set to 0" << std::endl;
 	a = 0;
 	std::cout << "a: " << a << std::endl;
 	a++;
-	std::cout << "a: " << a << std::endl;
-	std::cout << "a set to 0 and decreased by 1" << std::endl;
+	std::cout << "a++: " << a << std::endl;
+	std::cout << "a set to 0" << std::endl;
 	a = 0;
 	std::cout << "a: " << a << std::endl;
 	a--;
-	std::cout << "a: " << a << std::endl;
+	std::cout << "a--: " << a << std::endl;
+
 	std::cout << std::setw(20) << std::setfill('-') << "" << std::endl;
+	
+	std::cout << "a and b set to 1000" << std::endl;
+	a = 100;
+	b = 100;
+	a++;
+	std::cout << "a == b " << (a == b) << std::endl;
+	std::cout << "a: " << a.toFloat() << std::endl;
+	std::cout << "b: " << b.toFloat() << std::endl;
+	std::cout << "a (raw bits): " << a.getRawBits() << std::endl;
+	std::cout << "b (raw bits): " << b.getRawBits() << std::endl;
+
+	std::cout << std::setw(20) << std::setfill('-') << "" << std::endl;
+
 	std::cout << "a: " << a << std::endl;
 	std::cout << "b: " << b << std::endl;
 	std::cout << "c: " << c << std::endl;
@@ -75,5 +115,6 @@ int	main()
 	std::cout << "min(a, b)" << Fixed::min(a, b) << std::endl;
 	std::cout << "max(c, d)" << Fixed::max(c, d) << std::endl;
 	std::cout << "min(c, d)" << Fixed::min(c, d) << std::endl;
+
 	return (0);
 }
