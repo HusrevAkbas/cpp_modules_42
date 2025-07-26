8b7a86d0135ab39f9a3716cb0916d6fd18ee9034
HusrevAkbas <husrev.akbas@gmail.com>
2025-07-26 14:10:46 +0200

    test: module 1 ex02 more cases added to explain what a reference is

--- module_1/ex02/main.cpp
+++ module_1/ex02/main.cpp
@@ -3,40 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: husrevakbas <husrevakbas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 23:12:07 by husrevakbas       #+#    #+#             */
-/*   Updated: 2025/07/23 12:17:56 by huakbas          ###   ########.fr       */
+/*   Updated: 2025/07/26 14:03:52 by husrevakbas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+#define GREEN	"\e[1;32m"
+#define RESET	"\e[0m"
+
+void	test1(std::string	string)
+{
+	std::cout << std::endl << "Memory of address of string: " << &string << std::endl;
+	std::cout << "Value of address of string: " << string << std::endl;
+	string = "NEW BRAIN";
+	std::cout << "Memory of address of string: " << &string << std::endl;
+	std::cout << "Value of address of string: " << string << std::endl;
+}
+
+void	test2(std::string	*string)
+{
+	std::cout << std::endl << "Memory of address of string: " << string << std::endl;
+	std::cout << "Value of address of string: " << *string << std::endl;
+	*string = "NEW BRAIN";
+	std::cout << "Memory of address of string: " << string << std::endl;
+	std::cout << "Value of address of string: " << *string << std::endl;
+}
+
+void	test3(std::string	&string)
+{
+	std::cout << std::endl << "Memory of address of string: " << &string << std::endl;
+	std::cout << "Value of address of string: " << string << std::endl;
+	string = "NEW BRAIN";
+	std::cout << "Memory of address of string: " << &string << std::endl;
+	std::cout << "Value of address of string: " << string << std::endl;
+}
+/*
+	you can not reassign a reference type
+*/
+
 int	main()
 {
 	std::string	string;
 	std::string	*stringPTR = &string;
-	std::string	&stringREF = string;
+	std::string	&stringREF = string; // *(stringPTR);
 
 	string = "HI THIS IS BRAIN";
 
-	std::cout << std::endl << "Memory of address of string: " << &string << std::endl;
+	std::cout << GREEN << std::endl << "Memory of address of string: " << &string << std::endl;
 	std::cout << "Memory of address of stringPTR: " << stringPTR << std::endl;
 	std::cout << "Memory of address of stringREF: " << &stringREF << std::endl;
 	
 	std::cout << std::endl << "Value of address of string: " << string << std::endl;
 	std::cout << "Value of address of stringPTR: " << *stringPTR << std::endl;
-	std::cout << "Value of address of stringREF: " << stringREF << std::endl;
-	
-	string = "HI THIS IS NOT A BRAIN";
+	std::cout << "Value of address of stringREF: " << stringREF << RESET << std::endl;
 
-	std::cout << std::endl << "Memory of address of string: " << &string << std::endl;
+	test1(string);
+	// test2(stringPTR);
+	// test3(stringREF);
+	// test4
+	// std::string	string2 = "NEW BRAIN";
+	// stringPTR = &string2;
+	// stringREF = string2;
+	// string2 = "BRAND NEW BRAIN";
+
+	std::cout << GREEN << std::endl << "Memory of address of string: " << &string << std::endl;
 	std::cout << "Memory of address of stringPTR: " << stringPTR << std::endl;
 	std::cout << "Memory of address of stringREF: " << &stringREF << std::endl;
 	
 	std::cout << std::endl << "Value of address of string: " << string << std::endl;
 	std::cout << "Value of address of stringPTR: " << *stringPTR << std::endl;
-	std::cout << "Value of address of stringREF: " << stringREF << std::endl;
+	std::cout << "Value of address of stringREF: " << stringREF << RESET << std::endl;
 
+	// test2(string);
+	// test2(stringREF);
+	// test2(*stringPTR);
 	return (0);
 }
