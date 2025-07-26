ff0fef413d17fae3e0a25e91a96fb8ab32b21399
HusrevAkbas <husrev.akbas@gmail.com>
2025-07-26 13:01:48 +0200

    refactor: inclued guard added to Zombie.hpp

--- module_1/ex00/Zombie.hpp
+++ module_1/ex00/Zombie.hpp
@@ -3,12 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   Zombie.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: husrevakbas <husrevakbas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 22:09:55 by husrevakbas       #+#    #+#             */
-/*   Updated: 2025/07/22 19:33:17 by huakbas          ###   ########.fr       */
+/*   Updated: 2025/07/26 12:53:29 by husrevakbas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef	ZOMBIE_H
+# define	ZOMBIE_H
 
 #include <iostream>
 
@@ -22,3 +25,5 @@ class Zombie
 		~Zombie ( void );
 		void	announce( void );
 };
+
+#endif	//	ZOMBIE_H
