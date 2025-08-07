fbabc4007a08c00b61983dfb6dc5b65b43cff43b
Husrev Akbas <huakbas@c3r9p5.42vienna.com>
2025-08-07 12:06:58 +0200

    refactor: module 3 ex03 scav trap updated

--- module_3/ex03/ScavTrap.cpp
+++ module_3/ex03/ScavTrap.cpp
@@ -3,28 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ScavTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: husrevakbas <husrevakbas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 18:02:19 by huakbas           #+#    #+#             */
-/*   Updated: 2025/08/07 11:52:17 by huakbas          ###   ########.fr       */
+/*   Updated: 2025/08/07 00:29:24 by husrevakbas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
-ScavTrap::ScavTrap()
+ScavTrap::ScavTrap() : ClapTrap("DEFAULT")
 {
 	std::cout << WARN2 << "Default ScavTrap constructed" << RESET << std::endl;
-	this->_name = "DEFAULT";
 	this->_hit_points = 100;
 	this->_energy_points = 50;
 	this->_attak_damage = 20;
 }
 
-ScavTrap::ScavTrap( const std::string name)
+ScavTrap::ScavTrap( const std::string name) : ClapTrap(name)
 {
 	std::cout << WARN2 << "ScavTrap " << name << " is constructed" << RESET << std::endl;
-	this->_name = name;
 	this->_hit_points = 100;
 	this->_energy_points = 50;
 	this->_attak_damage = 20;
@@ -37,7 +35,6 @@ ScavTrap::~ScavTrap()
 
 ScavTrap::ScavTrap(const ScavTrap &other) : ClapTrap(other)
 {
-	// this = ClapTrap(other._name);
 	std::cout << WARN
 	<< "ScavTrap " << other._name << " replicated" << RESET << std::endl;
 	if (this != &other)
@@ -46,7 +43,7 @@ ScavTrap::ScavTrap(const ScavTrap &other) : ClapTrap(other)
 
 ScavTrap& ScavTrap::operator=(const ScavTrap &other)
 {
-	std::cout << WARN << "ScavTrap assignment operator called" << RESET << std::endl;
+	std::cout << WARN << "Assignment operator called" << RESET << std::endl;
 	if (this == &other)
 		return (*this);
 	this->_name = other._name;
@@ -58,6 +55,19 @@ ScavTrap& ScavTrap::operator=(const ScavTrap &other)
 
 void	ScavTrap::attack(const std::string &target)
 {
+	if (this->_hit_points <= 0)
+	{
+		std::cout << MAGENT << "ScavTrap " << this->_name
+		<< " is already dead. Can't attack. \\( X _ X )/" << RESET << std::endl;
+		return ;
+	}
+	if (this->_energy_points <= 0)
+	{
+		std::cout << MAGENT << "ScavTrap " << this->_name
+		<< " doesn't have any energy. Can't attack. /( @ _ @ )\\" << RESET << std::endl;
+		return ;
+	}
+	this->_energy_points--;
 	std::cout << MAGENT
 	<< "ScavTrap " << this->_name
 	<< " attacks " << target
@@ -68,30 +78,56 @@ void	ScavTrap::attack(const std::string &target)
 
 void	ScavTrap::guardGate()
 {
+	if (this->_energy_points <= 0)
+	{
+		std::cout << MAGENT << "ScavTrap " << this->_name
+		<< " doesn't have any energy. Can't switch to Gate Keeper mode. /( @ _ @ )\\" << RESET << std::endl;
+		return ;
+	}
+	this->_energy_points--;
 	std::cout << BLUE
 	<< "ScavTrap " << this->_name
-	<< " switched to Gate keeper mode "
+	<< " switched to Gate Keeper mode "
 	<< RESET << std::endl;
 }
 
 void	ScavTrap::takeDamage(unsigned int amount)
 {
-	// if (amount > static_cast<unsigned int>(this->_hit_points))
-	// {
-	// 	std::cout << RED << "ClapTrap " << this->_name
-	// 	<< " is dead. RIP" << RESET << std::endl;
-	// 	return ;
-	// }
+	if (this->_hit_points <= 0)
+	{
+		std::cout << YELLOW << "ScavTrap " << this->_name
+		<< " is already dead. You can't hurt it anymore." << RESET << std::endl;
+		return ;
+	}
 	this->_hit_points -= amount;
 	std::cout << YELLOW
 	<< "ScavTrap " << this->_name
 	<< " takes " << amount
 	<< " damage. (Current hit points: " << this->_hit_points << ")"
 	<< RESET << std::endl;
+	if (this->_hit_points <= 0)
+	{
+		std::cout << YELLOW << "ScavTrap " << this->_name
+		<< " is no more. \\( X _ X )/" << RESET << std::endl;
+		return ;
+	}
 }
 
 void	ScavTrap::beRepaired(unsigned int amount)
 {
+	if (this->_hit_points <= 0)
+	{
+		std::cout << GREEN << "ScavTrap " << this->_name
+		<< " is already dead. It can not be repaired. \\( X _ X )/" << RESET << std::endl;
+		return ;
+	}
+	if (this->_energy_points <= 0)
+	{
+		std::cout << GREEN << "ScavTrap " << this->_name
+		<< " does'nt have any energy. It can not be repaired. /( @ _ @ )\\ " << RESET << std::endl;
+		return ;
+	}
+	this->_energy_points--;
 	this->_hit_points += amount;
 	std::cout << GREEN
 	<< "ScavTrap " << this->_name
