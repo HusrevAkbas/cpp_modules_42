e3b9442e4554ff34c928f4fbb35751f27c428bfd
HusrevAkbas <husrev.akbas@gmail.com>
2025-08-31 16:38:33 +0200

    feat: module 9 ex01 reverse polish notation calculator added

---
+++ module_9/ex01/main.cpp
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: husrevakbas <husrevakbas@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/31 15:44:42 by husrevakbas       #+#    #+#             */
+/*   Updated: 2025/08/31 16:36:57 by husrevakbas      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <algorithm>
+#include <stack>
+
+#define	RED		"\e[1;31m"
+#define	RESET	"\e[0m"
+
+void	trim(std::string &str)
+{
+	str.erase(std::remove_if(str.begin(), str.end(), isspace), str.end());
+}
+
+bool	isoperator(char ch)
+{
+	std::string	operators = "+-*/";
+	if (operators.find(ch) != std::string::npos)
+		return (true);
+	return (false);
+}
+
+void	operate(char op, std::stack<int> *numbers)
+{
+	int	operand2 = numbers->top();
+	numbers->pop();
+	int operand = numbers->top();
+	numbers->pop();
+
+	//	COMMENT OUT TO SEE CALCULATION STEP BY STEP
+	//std::cout << RED << operand << " " << op << " " << operand2 << "\n" << RESET;
+
+	switch (op)
+	{
+	case '+':
+		numbers->push(operand + operand2);
+		break;
+	case '-':
+		numbers->push(operand - operand2);
+		break;
+	case '*':
+		numbers->push(operand * operand2);
+		break;
+	case '/':
+		numbers->push(operand / operand2);
+		break;
+	default:
+		break;
+	}
+}
+
+void	calculate(char op, std::stack<int>	*numbers)
+{
+	if (isdigit(op))
+		numbers->push(op - 48);
+	else if (numbers->size() < 2)
+	{
+		std::cerr << RED << "Error: not enough numbers to calculate!\n" << RESET;
+		exit(4);
+	}
+	else
+		operate(op, numbers);
+}
+
+int	main(int argc, char **argv)
+{
+
+	std::stack<int>	numbers;
+	
+	if (argc != 2)
+	{
+		std::cerr << RED << "Error: Program takes only one argument in quotes:\n" << RESET;
+		std::cout << "./RPN \"1 2 3 + -\"";
+		return (1);
+	}
+
+	std::string	input = argv[1];
+	trim(input);
+
+	// check invalid characters
+	if (input.find_first_not_of("1234567890+-*/") != std::string::npos)
+	{
+		std::cerr << RED << "Error: Invalid char in sequence: " << RESET
+		<< input[input.find_first_not_of("1234567890+-*/")] <<"\n";
+		return (2);
+	}
+
+	// check digits and operators count
+	if (std::count_if(input.begin(), input.end(), isdigit) != std::count_if(input.begin(), input.end(), isoperator) + 1)
+	{
+		std::cerr << RED << "Error: Digit count must be one greater than operator count\n" << RESET;
+		return (3);
+	}
+
+	// iterator applies function to all chars one by one
+	for (std::string::iterator i = input.begin(); i < input.end(); i++)
+		calculate(*i, &numbers);
+
+	// print output
+	std::cout << numbers.top() << "\n";
+	return (0);
+}
