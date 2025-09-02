4cb627b320a81a43ef50713d41d9467ed464c15d
Husrev Akbas <huakbas@c3r11p4.42vienna.com>
2025-09-02 16:19:35 +0200

    feat: module 9 ex02 main chain, pending and non participant elements set

--- module_9/ex02/main.cpp
+++ module_9/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 11:55:48 by huakbas           #+#    #+#             */
-/*   Updated: 2025/09/02 14:55:16 by huakbas          ###   ########.fr       */
+/*   Updated: 2025/09/02 16:18:32 by huakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,44 +81,131 @@ size_t	find_power_of_two_divider(size_t size)
 	return (divider);
 }
 
+void	swap_pairs(std::vector<int> *vec, size_t item_per_pair)
+{
+	std::vector<int>::iterator	it_first;
+	std::vector<int>::iterator	it_last;
+	std::vector<int>::iterator	it_last_of_second_pair;
+	std::vector<int>::iterator	it_last_of_first_pair;
+	std::vector<int>::iterator	it_swap;
+
+	for (size_t i = item_per_pair - 1; i + item_per_pair < vec->size(); i += item_per_pair * 2)
+	{
+		it_first = vec->begin() + i - item_per_pair + 1;
+		it_last = vec->begin() + i + 1;
+		it_last_of_first_pair = vec->begin() + i;
+		it_last_of_second_pair = vec->begin() + i + item_per_pair;
+		it_swap = it_last;
+		std::cout << "last of first: " << *it_last_of_first_pair << " last of sec: " << *it_last_of_second_pair << "\n";
+		std::cout << "first it: " << *it_first << " last it: " << *it_last << " swap begins: " << *it_swap <<  "\n";
+		if (*it_last_of_first_pair > *it_last_of_second_pair)
+			std::swap_ranges(it_first, it_last, it_swap);
+	}
+}
+
+void	insert_pend_to_main_chain(std::vector<int> *main_chain, std::vector<int> *pending, size_t item_per_pair)
+{
+	(void) main_chain;
+	(void) item_per_pair;
+	if (pending->size() == 0)
+		return ;
+}
+
 void	merge_insertion_sort_or_the_Ford_Johnson_algorithm
 		(std::vector<int> *vec, size_t item_per_pair)
 {
 	if (item_per_pair > vec->size() / 2)
 		return ;
 
-	std::cout << std::endl;
-	for (std::vector<int>::iterator i = vec->begin(); i != vec->end(); i++)
-	{
-		std::cout << *i << " ";
-	}
-	std::cout << std::endl;
+	// // uncomment to see the sequence
+	// std::cout << std::endl;
+	// std::cout << "pair for " << item_per_pair << "\n";
+	// for (std::vector<int>::iterator i = vec->begin(); i != vec->end(); i++)
+	// {
+	// 	std::cout << *i << " ";
+	// }
+	// std::cout << std::endl;
 
-	size_t	last_of_prev_pair;
-	size_t	last_of_next_pair;
-	std::vector<int>::iterator	it_first;
-	std::vector<int>::iterator	it_last;
-	std::vector<int>::iterator	it_swap;
+	swap_pairs(vec, item_per_pair);
 
-	for (size_t i = item_per_pair - 1; i + item_per_pair < vec->size(); i += item_per_pair * 2)
-	{
-		last_of_prev_pair = i;
-		last_of_next_pair = i + item_per_pair;
-		std::cout << last_of_prev_pair << " | " << last_of_next_pair << " - " << item_per_pair <<  "\n";
-		it_first = vec->begin() + i - item_per_pair + 1;
-		it_last = vec->begin() + i + 1;
-		it_swap = it_last;
-		std::cout << "first it: " << *it_first << " last it: " << *it_last << " swap begins: " << *it_swap <<  "\n";
-		std::swap_ranges(it_first, it_last, it_swap);
-	}
-	
-	for (std::vector<int>::iterator i = vec->begin(); i != vec->end(); i++)
-	{
-		std::cout << *i << " ";
-	}
-	std::cout << std::endl;
+	// //	uncomment to see the change
+	// for (std::vector<int>::iterator i = vec->begin(); i != vec->end(); i++)
+	// {
+	// 	std::cout << *i << " ";
+	// }
+	// std::cout << std::endl;
 
 	merge_insertion_sort_or_the_Ford_Johnson_algorithm(vec, item_per_pair * 2);
+
+	std::cout << "\ninsert for " << item_per_pair << "\n";
+	std::vector<int>	main_chain;
+	std::vector<int>	pending;
+	std::vector<int>	non_participant;
+
+	std::vector<int>::iterator	it_start;
+	std::vector<int>::iterator	it_end;
+	std::vector<int>::iterator	it_insert;
+
+	for (std::vector<int>::iterator i = vec->begin(); i != vec->end(); i++)
+	{
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+
+	// set main chain
+	main_chain.insert(main_chain.begin(), vec->begin(), vec->begin() + item_per_pair);
+	for (size_t i = item_per_pair; i + item_per_pair <= vec->size(); i += 2 * item_per_pair)
+	{
+		it_start = vec->begin() + i;
+		it_end = vec->begin() + i + item_per_pair;
+		main_chain.insert(main_chain.end(), it_start, it_end);
+	}
+
+	// set pending
+	for (size_t i = 2 * item_per_pair; i + item_per_pair <= vec->size(); i += 2 * item_per_pair)
+	{
+		it_start = vec->begin() + i;
+		it_end = vec->begin() + i + item_per_pair;
+		pending.insert(pending.end(), it_start, it_end);
+	}
+
+	// set non_participant items
+	non_participant.insert(non_participant.end(), it_end, vec->end());
+
+	std::cout << "main 0: ";
+	for (std::vector<int>::iterator i = main_chain.begin(); i != main_chain.end(); i++)
+	{
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "pendng: ";
+	for (std::vector<int>::iterator i = pending.begin(); i != pending.end(); i++)
+	{
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "nonpart: ";
+	for (std::vector<int>::iterator i = non_participant.begin(); i != non_participant.end(); i++)
+	{
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+
+	// insert pending to main chain using binary search
+	insert_pend_to_main_chain(&main_chain, &pending, item_per_pair);
+
+	// insert non participants to main chain
+	main_chain.insert(main_chain.end(), non_participant.begin(), non_participant.end());
+
+
+	std::cout << "main 1: ";
+	for (std::vector<int>::iterator i = main_chain.begin(); i != main_chain.end(); i++)
+	{
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+	// set vector to main chain so upper levels knows the change
+	// *vec = main_chain;
 }
 
 int	main(int argc, char **argv)
